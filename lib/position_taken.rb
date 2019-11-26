efdea03188b2652c2f640c4abe6594c78a9557f5
Angelo Poole <angelo.poole@outlook.com>
2019-11-26 08:58:46 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if index == " " || "" || nil
+  if index == " " || "" 
+    false
+  elsif index == nil
     false
   elsif index != "X" || "O"
     false
